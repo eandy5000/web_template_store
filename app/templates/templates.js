764302b334780f19b2786449644f2cff8b7ad39e
E. Andrew Eccleston <eandreweccleston@Es-MacBook-Pro.local>
2016-02-27 08:46:28 -0600

    working on seed

---
+++ app/templates/templates.js
@@ -0,0 +1,17 @@
+'use strict'
+
+angular.module('templateStore.templates', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider){
+
+  $routeProvider
+    .when('/templates', {
+      templateUrl: "templates/templates.html",
+      controller: "TemplatesCtrl"
+    })
+
+}])
+
+.controller('TemplatesCtrl',['$scope',function($scope){
+  console.log('templates wrking');
+}]);
